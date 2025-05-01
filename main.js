bef61107003125fdefccf038db8dc79f679df878
OrionGift3 <168460082+Shengelaa@users.noreply.github.com>
2025-05-02 00:31:09 +0400

    second done

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 // file name in the first one, then read this file and delete exact file.
 
 const fs = require("fs/promises");
+const path = require("path");
 
 async function main() {
   const dirs = await fs.readdir(__dirname);
@@ -22,4 +23,28 @@ async function main() {
 
 //Easy pirveli, imena cookie batono davit chavylape.
 
-main();
+//! main();
+
+//
+//create a files and folders in root directory,
+//  make some nested direcotirs with some files,
+// then write a script that deletes only .txt files from directories.
+
+async function foo1(fullPath) {
+  const dirs = await fs.readdir(fullPath);
+  for (let dir of dirs) {
+    const absolutePath = path.join(fullPath, dir);
+    const stat = await fs.stat(path.join(fullPath, dir));
+    if (stat.isDirectory()) {
+      console.log("dir: " + dir);
+
+      foo1(absolutePath);
+    } else if (path.extname(dir) === ".txt") {
+      await fs.unlink(absolutePath);
+    }
+  }
+}
+
+//! foo1(__dirname);
+
+//extname arvicodi nagdad 
