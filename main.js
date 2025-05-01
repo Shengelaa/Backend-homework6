85945cac6b851122b1d3504d3bf33d0b6bfea06c
OrionGift3 <168460082+Shengelaa@users.noreply.github.com>
2025-05-02 01:16:49 +0400

    Finished

--- main.js
+++ main.js
@@ -3,7 +3,8 @@
 
 const fs = require("fs/promises");
 const path = require("path");
-
+const moment = require("moment");
+const { ChildProcess } = require("child_process");
 async function main() {
   const dirs = await fs.readdir(__dirname);
   console.log(dirs);
@@ -47,4 +48,60 @@ async function foo1(fullPath) {
 
 //! foo1(__dirname);
 
-//extname arvicodi nagdad 
+//extname arvicodi nagdad
+
+//3) Print all files in a folder along with size and last modified time.
+//index.js - 2.1 KB - modified: 2024-01-01, you can use moment js.
+
+async function main1(fullPath) {
+  const dirs = await fs.readdir(fullPath);
+  for (let dir of dirs) {
+    const stat = await fs.stat(path.join(fullPath, dir));
+    if (stat.isDirectory()) {
+      continue;
+    } else {
+      const size = stat.size / 1024;
+      const lastModified = moment(stat.mtime).format("YYYY-MM-DD HH:mm:ss");
+      console.log(`${dir} - ${size} KB - Last modified at : ${lastModified}`);
+    }
+  }
+}
+
+//! main1(__dirname);
+
+//continue-ზე ჯპიტის ვკითხე
+//უაზრო ერორი მომცა მიყრიდა უაზრო ფაილებს
+//რაც შექმნილი არ მქონდა მე
+//რო ვკითხე ან გითჰაბის ან ნოუდ მოდულების გამოო
+
+//4) create a product.json files where should be some products with
+//  folowing properties: name, description, price, color, id. When you run next command "
+// node main.js ASC" it should return all sorted
+// products with price ascending. "node main.js  DESC"  => sorted products with price descending.
+
+async function main2() {
+  const [, , command] = process.argv;
+  const data = await fs.readFile("product.json", "utf-8");
+  const object = JSON.parse(data);
+
+  let array = [];
+
+  object.map((el) => {
+    if (command === "ASC") {
+      price = el.price;
+      array.push(el.price);
+      const res = array.sort((a, b) => a - b);
+      console.log(res, "Ascending");
+    } else if (command === "DESC") {
+      price = el.price;
+      array.push(el.price);
+      const res = array.sort((a, b) => b - a);
+      console.log(res, "Descending");
+    }
+  });
+}
+
+main2();
+
+//უმაგრესი დავალება იყო ბოლო მაგრად მომეწონა
+//თან სორტიც გავიხსენე იმენა W homework
